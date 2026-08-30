3de24367c7651954232a8ad866c6bb1cdbce8796
dsbitor <dsbanham@gmail.com>
2026-08-30 13:34:03 -0400

    gore 0.1.77: install the PRM PDF as a formula resource
    Adds a 'prm' resource fetching gore-prm.pdf from the same release,
    staged into #{doc} on install, with a caveats block telling the user
    where it landed. URLs/checksums bumped to v0.1.77.

--- Formula/gore.rb
+++ Formula/gore.rb
@@ -5,21 +5,26 @@ class Gore < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.74/gore-0.1.74-darwin-arm64.tar.gz"
-      sha256 "5a20695ae43d98ba692ae4ac9a356f7738934f97f1cf859d629d45686829d391"
+      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.77/gore-0.1.77-darwin-arm64.tar.gz"
+      sha256 "3305500882d6c640acc2bb3b4d00e2cf2e05a95e25c457c4e9c3750fbd945467"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.74/gore-0.1.74-linux-amd64.tar.gz"
-      sha256 "5f2154846e69a42882370ce975a4ed0a51cf69a363024e0361d64c78ca810d86"
+      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.77/gore-0.1.77-linux-amd64.tar.gz"
+      sha256 "6bd775d4aa0ca86cbeb9d866e2002d62fd7d2beb02fb1f21a6071453eaaedf5f"
     end
 
     on_arm do
-      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.74/gore-0.1.74-linux-arm64.tar.gz"
-      sha256 "5767da0cfd4caee8cb521a3c9e7f5113865203a45ee6533aab7fd8e0136ee40d"
+      url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.77/gore-0.1.77-linux-arm64.tar.gz"
+      sha256 "8914cef14a309880b4b0135aa77676b78ff210e616e8acb2d620194ea72b5b99"
     end
+  end
+
+  resource "prm" do
+    url "https://github.com/dsbitor/gore-releases/releases/download/v0.1.77/gore-prm.pdf"
+    sha256 "a7b3354435cff60af7190037a6e27a042f6d816d9ca0a36fca8de6103d821066"
   end
 
   def install
@@ -31,6 +36,17 @@ class Gore < Formula
       "gore-linux-amd64"
     end
     bin.install binary_name => "gore"
+
+    resource("prm").stage do
+      doc.install "gore-prm.pdf"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      The gore Programmer's Reference Manual (PDF) was installed to:
+        #{doc}/gore-prm.pdf
+    EOS
   end
 
   test do
