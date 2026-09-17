bba9438e4b720e0e98407447a0a404a652e337d2
David Banham <dsbanham@gmail.com>
2026-09-17 13:38:01 -0400

    gore 0.4.121: include() for sharing Starlark logic across scripts
    https://github.com/dsbitor/gore-releases/releases/tag/v0.4.121

--- Formula/gore.rb
+++ Formula/gore.rb
@@ -4,26 +4,26 @@ class Gore < Formula
   license :cannot_represent
 
   on_macos do
-    url "https://github.com/dsbitor/gore-releases/releases/download/v0.3.114/gore-0.3.114-darwin-arm64.tar.gz"
-    sha256 "158de65d6fd9fbd18125a64f53aca42bde22ea0ab9ee07e802e786c170c1233d"
+    url "https://github.com/dsbitor/gore-releases/releases/download/v0.4.121/gore-0.4.121-darwin-arm64.tar.gz"
+    sha256 "934a7a10eb088e4ee6d4203bca10cd39deccbde86aaeba0086aeb2eb24c223be"
     depends_on arch: :arm64
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dsbitor/gore-releases/releases/download/v0.3.114/gore-0.3.114-linux-amd64.tar.gz"
-      sha256 "ed4ac21b07aa172df6fe125416c826513b37ffda475d39abbcd89384dbf38579"
+      url "https://github.com/dsbitor/gore-releases/releases/download/v0.4.121/gore-0.4.121-linux-amd64.tar.gz"
+      sha256 "347c6636ab9f0bd4c6d3735b3ed722284e47486e463b4d2582b7072d2710d602"
     end
 
     on_arm do
-      url "https://github.com/dsbitor/gore-releases/releases/download/v0.3.114/gore-0.3.114-linux-arm64.tar.gz"
-      sha256 "ba8a4600f090a900e342e944a4ee9b54573b70d869975919f3be8af749e6067b"
+      url "https://github.com/dsbitor/gore-releases/releases/download/v0.4.121/gore-0.4.121-linux-arm64.tar.gz"
+      sha256 "1d96865bd4dfeef686b94e85c524bbb230a4dcf092dbbd252f89249ad49854ad"
     end
   end
 
   resource "prm" do
-    url "https://github.com/dsbitor/gore-releases/releases/download/v0.3.114/gore-prm.pdf"
-    sha256 "818c0bee144f52c5acd8f471d5ed9adf250e2c2657344551247ea67185a04e0d"
+    url "https://github.com/dsbitor/gore-releases/releases/download/v0.4.121/gore-prm.pdf"
+    sha256 "2ac3d5fd16bf9235b87db31c5c5bd9ba27d4a12a71695d975758c12c355166c0"
   end
 
   def install
